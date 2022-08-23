51d205ea33116602d3133bc7cbe22bd4c4b1c8ee
Ryan Ritchey <rritchey@esri.com>
2022-08-22 19:26:14 -0700

    Implement checkout page / items and cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import './cart-dropdown.styles.scss'
 import { useContext } from 'react'
+import {useNavigate} from 'react-router-dom'
 import { CartContext } from '../../context/cart.context'
 
 import CartItem from '../cart-item/cart-item.component'
@@ -9,12 +10,17 @@ import Button from '../button/button.component'
 
 const CartDropdown = ()=>{
     const {cartItems} = useContext(CartContext)
+    const navigate = useNavigate()
+    const goToCheckout = ()=>{
+        navigate('/checkout')
+    }
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map((item)=><CartItem key={item.id} cartItem={item}/>)}
             </div>
-        <Button>CHECKOUT</Button>
+        <Button onClick={goToCheckout}>CHECKOUT</Button>
             
         </div>
     )
